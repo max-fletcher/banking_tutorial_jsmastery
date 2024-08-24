554cf99834332c7a2e76a4d6cb6986a5a8368fb0
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-08-24 23:36:04 +0600

    Done upto 5:45:19. Completed "My Banks" section(see sidebar).
    In the middle of implementing "Transfer Funds".

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,6 +1,38 @@
-const MyBanks = () => {
+import HeaderBox from "@/components/HeaderBox"
+import { getLoggedInUser } from "@/../lib/actions/user.actions";
+import { getAccounts } from "@/../lib/actions/bank.actions";
+import BankCard from "@/components/BankCard";
+
+const MyBanks = async () => {
+  const loggedIn = await getLoggedInUser(); //NOTE: Calling this here since calling it inside AuthForm(i.e a client component) is not possible
+  const accounts = await getAccounts({userId: loggedIn.$id}); // passing loggedIn user's '$id' renamed as 'userId'
+
+  console.log(accounts);
+
   return (
-    <div>MyBanks</div>
+    <section className="flex">
+      <div className="my-banks">
+        <HeaderBox 
+          title="My Bank Accounts" 
+          subtext="Effortlessly manage your banking activities."
+        />
+
+        <div className="space-y-4">
+          <h2 className="header-2">
+            Your cards
+          </h2>
+          <div className="flex flex-wrap gap-6">
+            {accounts && accounts.data.map((account: Account) => (
+                <BankCard 
+                  key={accounts.id}
+                  account={account}
+                  userName={loggedIn.firstName} 
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,6 +1,17 @@
+import HeaderBox from "@/components/HeaderBox"
+
 const Transfer = () => {
   return (
-    <div>Transfer</div>
+    <section className="payment-transfer">
+      <HeaderBox 
+        title="Payment Transfer"
+        subtext="Please provide any specific details or notes related to the payment transfer." 
+      />
+
+      <section className="size-full pt-5">
+        {/* Payment Transfer Form */}
+      </section>
+    </section>
   )
 }
 
