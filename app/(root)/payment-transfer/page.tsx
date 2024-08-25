f39267d1feea4ccb8bbc60296f178cede5993c87
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-08-25 17:56:29 +0600

    Done upto 5:52:00. Created transaction actions but need to create the collection in appwrite.

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,6 +1,16 @@
 import HeaderBox from "@/components/HeaderBox"
+import PaymentTransferForm from "@/components/PaymentTransferForm"
+import { getLoggedInUser } from "@/../lib/actions/user.actions";
+import { getAccounts } from "@/../lib/actions/bank.actions";
 
-const Transfer = () => {
+const Transfer = async () => {
+  const loggedIn = await getLoggedInUser(); //NOTE: Calling this here since calling it inside AuthForm(i.e a client component) is not possible
+  const accounts = await getAccounts({userId: loggedIn.$id}); // passing loggedIn user's '$id' renamed as 'userId'
+
+  if(!accounts) return;
+
+  const accountsData = accounts?.data // This is to avoid repeating accounts?.data over and over again below
+
   return (
     <section className="payment-transfer">
       <HeaderBox 
@@ -9,7 +19,7 @@ const Transfer = () => {
       />
 
       <section className="size-full pt-5">
-        {/* Payment Transfer Form */}
+        <PaymentTransferForm accounts={accountsData} />
       </section>
     </section>
   )
